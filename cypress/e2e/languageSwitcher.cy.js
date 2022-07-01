b9ca05459d3796a886f63adeeab249c6efc57b7e
Artem <artem.mindsadyrov@actum.digital.com>
2022-07-01 10:47:17 +0200

    update const in switcher

--- cypress/e2e/languageSwitcher.cy.js
+++ cypress/e2e/languageSwitcher.cy.js
@@ -1,9 +1,13 @@
-import BasePage from "../../page-objects/BasePage"
-import Navbar from "../../page-objects/components/Navbar"
-import LoginPage from "../../page-objects/pages/LoginPage"
+import BasePage from "../../page-objects/basePage"
+import Navbar from "../../page-objects/components/navbar"
+import LoginPage from "../../page-objects/pages/loginPage"
 import { url, login_password, login_username } from "/config"
 
 describe("Language Switcher Functionality", () => {
+	const loginPage = new LoginPage()
+	const navbar = new Navbar()
+	const basePage = new BasePage()
+
 	before(function () {
 		cy.visit(url)
 		BasePage.acceptCookies()
