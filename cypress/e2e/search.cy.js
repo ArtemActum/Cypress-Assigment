2d44e012794ba38e408479fbf127161eeb181047
Artem <artem.mindsadyrov@actum.digital.com>
2022-07-02 15:02:48 +0200

    new set prettier

--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -2,31 +2,38 @@ import BasePage from "../../page-objects/basePage"
 import Navbar from "../../page-objects/components/navbar"
 import LoginPage from "../../page-objects/pages/loginPage"
 import { url, login_password, login_username } from "/config"
+import SearchPage from "../../page-objects/pages/searchPage"
 
 describe("Search tests", () => {
 	const loginPage = new LoginPage()
 	const navbar = new Navbar()
 	const basePage = new BasePage()
+	const searchPage = new SearchPage()
 
 	before(function () {
 		cy.visit(url)
 		basePage.acceptCookies()
 	})
 
-	it("check if search is not case sensitive", () => {
-		navbar.search("Ralston Crawford")
-		cy.url().should("include", "/mychristies/activities")
-		navbar.search("rAlStON cRaWford")
-	})
+	// it("check if search is working", () => {
+	// 	navbar.search("Ralston Crawford")
+	// 	cy.get(search.searchResultItems).should("have.length.at.least", 1)
+	// })
+
+	// it("check if search is not case sensitive", () => {
+	// 	navbar.search("rAlStON cRaWford")
+	// 	cy.get(".chr-lot-tile__link").contains("Ralston Crawford")
+	// })
 
 	it("check if magnifying glass is working", () => {
 		navbar.searchWithMagnifyingGlass("Ralston Crawford")
+		cy.get(searchPage.searchResultItems).should("have.length.at.least", 1)
 	})
 
-	it("some negative tests", () => {
-		navbar.search("<script></script>")
-		navbar.search("%")
-		navbar.search("#")
-		navbar.search(" ") // 1 space
-	})
+	// it("some negative tests", () => {
+	// 	navbar.search("<script></script>")
+	// 	navbar.search("%")
+	// 	navbar.search("#")
+	// 	navbar.search(" ") // 1 space
+	// })
 })
---
+++ page-objects/pages/searchPage.js
@@ -0,0 +1,5 @@
+import BasePage from "../basePage"
+
+export default class searchPage extends BasePage {
+	searchResultItems = ".chr-lot-tile"
+}
