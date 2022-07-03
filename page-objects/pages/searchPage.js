2345b9cad1b67187898488d0a752e1a6df57b64b
Artem <artem.mindsadyrov@actum.digital.com>
2022-07-03 14:20:41 +0200

    set POM

--- page-objects/pages/searchPage.js
+++ page-objects/pages/searchPage.js
@@ -1,6 +1,7 @@
 import BasePage from '../basePage'
 
 export default class searchPage extends BasePage {
+  // Elements
   searchResultItems = '.chr-lot-tile'
   noResultsFoundDiv = '.chr-search-results__no-results.container-fluid'
 }
