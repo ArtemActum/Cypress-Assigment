66f4ea5db6d6d657a41cdeae0e51f8c960ba927a
Artem <artem.mindsadyrov@actum.digital.com>
2022-07-04 09:55:07 +0200

    add new page object

---
+++ page-objects/pages/forgottenPassPage.js
@@ -0,0 +1,8 @@
+import BasePage from '../basePage'
+
+export default class ForgottenPassPage extends BasePage {
+  // Elements
+  emailInput = "input[name='ctl00$MainContent$txtemailsection_emailaddress']"
+  continueBtn = '.btn-group'
+  // Page Object Methods
+}
