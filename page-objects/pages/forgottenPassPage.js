3600925340070d5a674bdaa1506dea26bcd67557
Artem <artem.mindsadyrov@actum.digital.com>
2022-07-08 11:13:36 +0200

    add new pageTitle element

--- page-objects/pages/forgottenPassPage.js
+++ page-objects/pages/forgottenPassPage.js
@@ -4,5 +4,7 @@ export default class ForgottenPassPage extends BasePage {
   // Elements
   emailInput = "input[name='ctl00$MainContent$txtemailsection_emailaddress']"
   continueBtn = '.btn-group'
+  pageTitleSpan = 'h1 > span'
+
   // Page Object Methods
 }
