d7c07613ed8552b4e4ca1005a159db2376c0da4c
Artem <artem.mindsadyrov@actum.digital.com>
2022-07-08 12:50:21 +0200

    add beforeEach

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,27 +1,27 @@
-import { url, login_password, login_username } from "/config"
-import LoginPage from "../../page-objects/pages/loginPage"
-import Navbar from "../../page-objects/components/navbar"
-import BasePage from "../../page-objects/basePage"
+import { url, login_password, login_username } from '/config'
+import LoginPage from '../../page-objects/pages/loginPage'
+import Navbar from '../../page-objects/components/navbar'
+import BasePage from '../../page-objects/basePage'
 
-describe("Login Functionality Tests", () => {
-	const loginPage = new LoginPage()
-	const navbar = new Navbar()
-	const basePage = new BasePage()
+describe('Login Functionality Tests', () => {
+  const loginPage = new LoginPage()
+  const navbar = new Navbar()
+  const basePage = new BasePage()
 
-	before(function () {
-		cy.visit(url)
-		basePage.acceptCookies()
-		cy.viewport(1080, 1920)
-	})
+  beforeEach(() => {
+    cy.visit(url).then(() => {
+      basePage.setCookie()
+    })
+  })
 
-	it("login into application", () => {
-		loginPage.login(login_username, login_password)
-		navbar.myAccount()
-		cy.url().should("include", "/mychristies/activities")
-	})
+  it('login into application', () => {
+    loginPage.login(login_username, login_password)
+    navbar.myAccount()
+    cy.url().should('include', '/mychristies/activities')
+  })
 
-	it("should try to login with invalid credentials", () => {
-		loginPage.login("invalid username", "invalid password")
-		loginPage.displayErrorMessage()
-	})
+  it('should try to login with invalid credentials', () => {
+    loginPage.login('invalid username', 'invalid password')
+    loginPage.displayErrorMessage()
+  })
 })
