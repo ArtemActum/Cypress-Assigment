376f2ee957a20014a2385b8a8f9e9f132858b600
Artem <artem.mindsadyrov@actum.digital.com>
2022-07-08 15:28:25 +0200

    fix search tests

--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -17,26 +17,27 @@ describe('Search tests', () => {
   })
 
   it('check if search is working', () => {
-    navbar.search('Ralston Crawford')
-    cy.url().should('include', 'Ralston')
+    navbar.search('Test')
+    cy.url().should('include', 'Test')
     cy.get(searchPage.searchResultItems).should('have.length.at.least', 1)
   })
 
   it('check if search is not case sensitive', () => {
-    navbar.search('rAlStON cRaWford')
-    cy.url().should('include', 'Ralston Crawford')
+    navbar.search('raLstOn CraWfOrd')
+    cy.get(searchPage.soldLots).click()
+    //cy.url().should('include', 'raLstOn CraWfOrd')
     cy.get('.chr-lot-tile__link').contains('Ralston Crawford')
   })
 
   it('check if magnifying glass is working', () => {
-    navbar.searchWithMagnifyingGlass('Ralston Crawford')
+    navbar.searchWithMagnifyingGlass('Test')
     cy.get(searchPage.searchResultItems).should('have.length.at.least', 1)
   })
 
   it('special chars', () => {
     navbar.search('%')
     cy.get(searchPage.searchResultItems).should('have.length', 0)
-    navbar.search(' ') // 1 space
-    cy.get(searchPage.searchResultItems).should('have.length', 0)
+    navbar.search('+') // 1 space
+    cy.get(searchPage.noResultsFoundDiv).should('be.visible')
   })
 })
