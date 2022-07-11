f73f2e829dd25327e528807cb8d4437d0dd2a78b
Artem <artem.mindsadyrov@actum.digital.com>
2022-07-11 11:24:43 +0200

    add baseurl

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://staging.christies.com/',
     defaultCommandTimeout: 10000,
     viewportWidth: 1920,
     viewportHeight: 1080,
