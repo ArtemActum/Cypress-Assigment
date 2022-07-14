1aad21ea9dbefb7de64e172a098390909bc13b79
Artem <artem.mindsadyrov@actum.digital.com>
2022-07-14 10:26:52 +0200

    new data

--- config.js
+++ config.js
@@ -1,2 +1,6 @@
-export const login_username = "artemminsadyrov@seznam.cz"
-export const login_password = "Zaqxsw111"
+export const login_username = 'artemminsadyrov@seznam.cz'
+export const login_password = 'Zaqxsw111'
+
+export const city = 'Prague'
+export const address = 'Argentinska'
+export const postalCode = '10600'
