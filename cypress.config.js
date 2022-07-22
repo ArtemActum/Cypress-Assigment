34f236106dd7c6bf5b62102d18c5dd18d1d9ff42
Artem <artem.mindsadyrov@actum.digital.com>
2022-07-22 17:02:04 +0200

    add blockHosts

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,7 @@ module.exports = defineConfig({
     defaultCommandTimeout: 10000,
     viewportWidth: 1920,
     viewportHeight: 1080,
+    blockHosts: ['static.srcspot.com'],
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
