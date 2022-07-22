e816e48c65619f28f4273ee56d31da1a429a6df1
Artem <artem.mindsadyrov@actum.digital.com>
2022-07-22 17:01:26 +0200

    commented "uncaught:exception"

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -12,11 +12,13 @@
 // You can read more here:
 // https://on.cypress.io/configuration
 // ***********************************************************
-Cypress.on('uncaught:exception', (err, runnable) => {
-  // returning false here prevents Cypress from
-  // failing the test
-  return false
-})
+
+// Cypress.on('uncaught:exception', (err, runnable) => {
+//   // returning false here prevents Cypress from
+//   // failing the test
+//   return false
+// })
+
 // Import commands.js using ES2015 syntax:
 import './commands'
 
