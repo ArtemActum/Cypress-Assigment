718f230976131903e6b63f7d7fa06f5784ff78b5
Artem <artem.mindsadyrov@actum.digital.com>
2022-07-29 13:24:48 +0200

    setup cucumber

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,18 @@
 const { defineConfig } = require('cypress')
+// const cucumber = require('cypress-cucumber-preprocessor').default
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://staging.christies.com/',
-    defaultCommandTimeout: 10000,
+    defaultCommandTimeout: 15000,
     viewportWidth: 1920,
     viewportHeight: 1080,
+    specPattern: ['**/*.feature', 'cypress/e2e/**/*.cy.{js,ts,tsx}'],
     blockHosts: ['static.srcspot.com'],
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
+      // on('file:preprocessor', cucumber())
       return require('./cypress/plugins/index.js')(on, config)
     },
   },
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,9 +1,12 @@
-Cypress.Commands.add("isVisible", selector => {
-	cy.get(selector).should("be.visible")
+Cypress.Commands.add('isVisible', (selector) => {
+  cy.get(selector).should('be.visible')
 })
 
-Cypress.Commands.add("login", (username, password) => {
-	cy.get("#username").type(username)
-	cy.get("#password").type(password)
-	cy.contains("Sign in").click()
+Cypress.Commands.add('login', (username, password) => {
+  cy.get('#username').type(username)
+  cy.get('#password').type(password)
+  cy.contains('Sign in').click()
 })
+
+// type definitions for Cypress object "cy"
+/// <reference types="cypress" />
