377b47852fce0222d3c4aafed569587677da6c67
Artem <artem.mindsadyrov@actum.digital.com>
2022-08-11 11:02:42 +0200

    delete config file

--- config.js
+++
@@ -1,6 +0,0 @@
-export const loginUsername = 'artemminsadyrov@seznam.cz'
-export const loginPassword = 'Zaqxsw111'
-
-export const city = 'Prague'
-export const address = 'Argentinska'
-export const postalCode = '10600'
