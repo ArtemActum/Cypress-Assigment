aae6c6711bd723531d30fb9ecef2463ff8be55d4
Artem <artem.mindsadyrov@actum.digital.com>
2022-08-16 17:10:09 +0200

    add setCookie command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -8,5 +8,23 @@ Cypress.Commands.add('login', (username, password) => {
   cy.contains('Sign in').click()
 })
 
+Cypress.Commands.add('setCookie', () => {
+  const date = new Date().toISOString()
+  cy.log(window.location.toString())
+  cy.setCookie('FastSignup', 'FastSignupCreated')
+  cy.setCookie('OptanonAlertBoxClosed', date).then(() => {
+    cy.reload()
+  })
+})
+
+// setCookie() {
+//   const date = new Date().toISOString()
+//   console.log(window.location)
+//   cy.setCookie('FastSignup', 'FastSignupCreated')
+//   cy.setCookie('OptanonAlertBoxClosed', date).then(() => {
+//     cy.reload()
+//   })
+// }
+
 // type definitions for Cypress object "cy"
 /// <reference types="cypress" />
