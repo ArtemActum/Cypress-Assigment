29056b4f472839ef9d4891aac6869fba2c2063a3
Artem <artem.mindsadyrov@actum.digital.com>
2022-08-19 13:19:29 +0200

    transform test from js to ts

---
+++ cypress/e2e/tests/languageSwitcher.cy.ts
@@ -0,0 +1,27 @@
+import HomePage from '../../page-objects/pages/homePage'
+
+describe('Language Switcher Functionality', () => {
+  const homePage = new HomePage()
+
+  beforeEach(() => {
+    homePage.open()
+    // cy.visit('/').then(() => {
+    //   homePage.setCookie()
+    // })
+  })
+
+  it('Switch ZH language', () => {
+    //homePage.switchlanguageZh()
+    homePage.switchLanguageTo('zh')
+  })
+
+  it('Switch Zh-Cn language', () => {
+    //homePage.switchlanguageZhCn()
+    homePage.switchLanguageTo('zh-cn')
+  })
+
+  it.only('Switch En language', () => {
+    //homePage.switchlanguageEn()
+    homePage.switchLanguageTo('en')
+  })
+})
