02868a7585a4588ed452d6d286ba0a637c5a7db1
Artem <artem.mindsadyrov@actum.digital.com>
2022-08-25 15:17:00 +0200

    change the extension to ts

--- cypress/e2e/tests/languageSwitcher.cy.ts
+++ cypress/e2e/tests/languageSwitcher.cy.ts
@@ -11,17 +11,17 @@ describe('Language Switcher Functionality', () => {
   })
 
   it('Switch ZH language', () => {
-    //homePage.switchlanguageZh()
+    // homePage.switchlanguageZh()
     homePage.switchLanguageTo('zh')
   })
 
   it('Switch Zh-Cn language', () => {
-    //homePage.switchlanguageZhCn()
+    //  homePage.switchlanguageZhCn()
     homePage.switchLanguageTo('zh-cn')
   })
 
   it.only('Switch En language', () => {
-    //homePage.switchlanguageEn()
+    // homePage.switchlanguageEn()
     homePage.switchLanguageTo('en')
   })
 })
