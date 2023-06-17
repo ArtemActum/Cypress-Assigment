e40754c6f5dcdbdbc901c7f6c315829b1d9ad921
WMC\artem.mindsadyrov <artem.mindsadyrov@actumdigital.com>
2023-06-17 17:37:27 +0200

    minor changes

--- cypress/page-objects/pages/homePage.ts
+++ cypress/page-objects/pages/homePage.ts
@@ -1,6 +1,9 @@
 import BasePage from '../basePage'
 
 export default class HomePage extends BasePage {
+  static clickAuctions() {
+    throw new Error('Method not implemented.')
+  }
   // Elements
   logo = '.chr-header__link-site  svg'
   signIn = '.chr-header__user-zone .chr-button__text'
