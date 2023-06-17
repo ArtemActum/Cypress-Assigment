b382a4ef63724093b4564f815c1d96bba63cc443
WMC\artem.mindsadyrov <artem.mindsadyrov@actumdigital.com>
2023-06-17 17:39:11 +0200

    added steps for MyC feature

---
+++ cypress/support/step_definitions/myAccount.ts
@@ -0,0 +1,40 @@
+/* eslint-disable new-cap */
+import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor'
+import MyAccount from '../../page-objects/pages/myAccountPage'
+import HomePage from '../../page-objects/pages/homePage'
+
+const homePage = new HomePage()
+const myAccountPage = new MyAccount()
+
+// before(function () {
+//   cy.log('accept cookie being set')
+//   cy.setCookie('FastSignup', 'FastSignupCreated', { domain: 'christies.com' })
+//   cy.setCookie('OptanonAlertBoxClosed', new Date().toString(), { domain: 'christies.com' })
+// })
+
+Given('I log in to MyC', () => {
+  cy.visit('/').then(() => {
+    homePage.setCookie()
+    homePage.login()
+    homePage.clickMyAccount()
+    myAccountPage.checkNameUser()
+    cy.url().should('include', '/mychristies/activities')
+  })
+})
+
+When('I view the submenu sections.',() => {
+    myAccountPage.checkBuyingSections()
+    myAccountPage.checkSellingSections()
+    myAccountPage.checkInterestSections()
+    myAccountPage.checkSettingsSections()
+})
+
+Then('The sections should be clickable and be displayed correctly.', () => {
+    myAccountPage.ClickableBuyingSection()
+    myAccountPage.ClickableSellingSection()
+    myAccountPage.ClickableInterestsSection()
+    myAccountPage.ClickableSettingsSection()
+})
+
+
+
